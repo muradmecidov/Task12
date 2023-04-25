01fe360342ad11081f405083e3a5e79d5949c498
muradmecidov <muradmecidov1499@gmail.com>
2023-04-25 12:02:10 +0400

    Task12

---
+++ ConsoleApp9/NewFolder/User.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace ConsoleApp9.NewFolder
+{
+    internal class User
+    {
+        private int Id {set;get;}   
+        private string Name
+        {
+            get; //{ return Name; }//
+            set;
+            //{
+            //    if (value.Length > 3)
+            //    {
+            //        Name = value;
+            //    }
+            //}
+        }
+        private string Surname { get;/*{ return Surname; }*/ set;/* { Surname = value; }*/ }
+        private static int Age
+        {
+            get; /*{ return Age; }*/
+            set;
+            //{
+            //    if (value > 0)
+            //    {
+            //        Age = value;
+            //    }
+            //}
+        }
+
+        public void GetFullName()
+        {
+            Console.WriteLine(Name + " " + Surname);
+
+        }
+        public static void ChangeAge(int age)
+        {
+            Age = age;
+            Console.WriteLine(Age);
+        }
+
+    }
+}
---
+++ ConsoleApp9/Program.cs
@@ -0,0 +1,49 @@
+﻿using ConsoleApp9.NewFolder;
+using System.Reflection;
+
+namespace ConsoleApp9
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Type type = typeof(User);
+            //PropertyInfo[]properties = type.GetProperties(BindingFlags.Instance | BindingFlags.Public | BindingFlags.NonPublic); 
+            //foreach (PropertyInfo property in properties)
+            //{
+
+            //    Console.WriteLine(property.Name); 
+            //}
+
+
+
+            User user = new User(); 
+            PropertyInfo propId=type.GetProperty("Id",BindingFlags.Instance|BindingFlags.NonPublic);
+            propId.SetValue(user, 10);
+            Console.WriteLine(propId.GetValue(user));
+            PropertyInfo propName = type.GetProperty("Name", BindingFlags.Instance | BindingFlags.NonPublic);
+            propName.SetValue(user, "Murad");
+            Console.WriteLine(propName.GetValue(user));
+            PropertyInfo propSurname = type.GetProperty("Surname", BindingFlags.Instance | BindingFlags.NonPublic);
+            propSurname.SetValue(user, "Mecidov");
+            Console.WriteLine(propSurname.GetValue(user));
+            PropertyInfo propAge = type.GetProperty("Age", BindingFlags.Static | BindingFlags.NonPublic);
+            propAge.SetValue(user, 21);
+            Console.WriteLine(propAge.GetValue(user));
+
+            MethodInfo getFullNameMethod = type.GetMethod("GetFullName", BindingFlags.Public | BindingFlags.Instance);
+            getFullNameMethod.Invoke(user, null);
+
+            MethodInfo changeAgeMethod = type.GetMethod("ChangeAge", BindingFlags.Public | BindingFlags.Static);
+            changeAgeMethod.Invoke(user, new object[] {30} );
+
+
+
+
+            // Assembly assembly = Assembly.GetExecutingAssembly();
+
+
+
+        }
+    }
+}
